76a6f358e3bd540e135ccbc9b31cad34ea433f74
Maxim <ivanovmax2002@yandex.ru>
2024-01-28 09:46:12 +0300

    some init fix in directory, install & config linters

---
+++ src/components/Header/ui/index.tsx
@@ -0,0 +1,8 @@
+import { FC } from 'react';
+
+import styles from './styles/Header.module.scss';
+const Index: FC = () => {
+  return <div className={styles.header}></div>;
+};
+
+export default Index;
